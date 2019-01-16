eb1e04e75c640781bc6f437eceb6beabd706d7b6
skopun <svenkopun@gmail.com>
2019-01-16 22:17:04 +0100

    initial commit

---
+++ src/main/java/edu/rit/iste500/dubai/RapidsCemeteryAPI/RapidsCemeteryApiApplication.java
@@ -0,0 +1,14 @@
+package edu.rit.iste500.dubai.RapidsCemeteryAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidsCemeteryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidsCemeteryApiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/edu/rit/iste500/dubai/RapidsCemeteryAPI/RapidsCemeteryApiApplicationTests.java
@@ -0,0 +1,17 @@
+package edu.rit.iste500.dubai.RapidsCemeteryAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RapidsCemeteryApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
